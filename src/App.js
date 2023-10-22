dbe30e098ed7e3e406cf4c0135979a7059528c95
elver <joelros2003@gmail.com>
2023-10-22 14:39:35 +0200

    react app?

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from './Button';
+
+function App() {
+  return (
+    <div className="App">
+        <Button name="game"/>
+        <Button name="game2"/>
+    </div>
+  );
+}
+
+export default App;
